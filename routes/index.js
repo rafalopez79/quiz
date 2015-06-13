5f202e57c87223fe8e764f35b4586326c269f3ff
rafalopez79 <rafalopez79@gmail.com>
2015-06-13 17:25:38 +0200

    Creditos

--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,11 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz' });
 });
 
+router.get('/author', function(req, res) {
+  res.render('authors');
+});
+
+
 router.get('/quizes/question', quizController.question);
 router.get('/quizes/answer',   quizController.answer);
 
