3c844adb066f6848bc7a9ee4628dab94eaec3a30
rafalopez79 <rafalopez79@gmail.com>
2015-06-13 13:26:45 +0200

    First page and favicon

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Bienvenido a Quiz', description: 'El portal d&oacute;nde podr&aacute;s crear tus propios juegos!' });
+  res.render('index', { title: 'Quiz' });
 });
 
 module.exports = router;
