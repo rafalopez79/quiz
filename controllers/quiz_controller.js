03a525d20795a36baf1ecb233ad2496b81675a5c
rafalopez79 <rafalopez79@gmail.com>
2015-06-14 14:34:28 +0200

    Post form

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -23,6 +23,7 @@ exports.answer = function(req, res){
         res.render('quizes/answer', {quiz: req.quiz, respuesta: 'Incorrecto'});
     };        
 };
+
 exports.index = function(req, res){   
     var search = req.query.search;
     if (search){
@@ -36,4 +37,14 @@ exports.index = function(req, res){
            res.render('quizes/index', { quizes: quizes});
         }).catch(function(error){ next(error);});
     }
+};
+
+exports.new = function(req, res){   
+    var quiz = models.Quiz.build({pregunta: 'Pregunta', respuesta: 'Respuesta'});
+    res.render('quizes/new', {quiz: quiz});         
+};
+
+exports.create = function(req, res){
+    var quiz = models.Quiz.build(req.body.quiz);
+    quiz.save({fields: ["pregunta","respuesta"]}).then(function(){res.redirect('/quizes');});
 };
--- routes/index.js
+++ routes/index.js
@@ -14,7 +14,9 @@ router.get('/author', function(req, res) {
 router.param('quizId', quizController.load);
 
 router.get('/quizes', quizController.index);
+router.get('/quizes/new', quizController.new);
 router.get('/quizes/:quizId(\\d+)', quizController.show);
 router.get('/quizes/:quizId(\\d+)/answer',   quizController.answer);
+router.post('/quizes/create', quizController.create);
 
 module.exports = router;
