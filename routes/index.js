b23c58e2ee259eb5f81f8e63b45f19fbbaa9be17
rafalopez79 <rafalopez79@gmail.com>
2015-06-21 11:11:20 +0200

    Adding comments

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var quizController = require('../controllers/quiz_controller.js');
+var commentController = require('../controllers/comment_controller.js');
 
 /* GET home page. */
 router.get('/', function(req, res) {
@@ -21,5 +22,9 @@ router.post('/quizes/create',               quizController.create);
 router.get('/quizes/:quizId(\\d+)/edit',    quizController.edit);
 router.put('/quizes/:quizId(\\d+)',         quizController.update);
 router.delete('/quizes/:quizId(\\d+)',      quizController.destroy);
+
+
+router.get('/quizes/:quizId(\\d+)/comments/new/',   commentController.new);
+router.post('/quizes/:quizId(\\d+)/comments/',       commentController.create);
 
 module.exports = router;
